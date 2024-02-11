55f7edacd55be3bb1c0507c889e2784355726ecc
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-02-11 10:47:24 -0300

    add is_fibonnaci

--- fibonnaci.py
+++ fibonnaci.py
@@ -4,8 +4,21 @@ listNumber = [0]
 def fibonnaci(qtdNumbersToCalculate, number = 1):
     listNumber.append(number)
     if listNumber.__len__() < qtdNumbersToCalculate:
-        print(number)
         fibonnaci(qtdNumbersToCalculate, number + listNumber[-2])
-    print(listNumber)
+    return listNumber
+
+"A number is Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square (Source: https://en.wikipedia.org/wiki/Fibonacci_number#Recognizing_Fibonacci_numbers)"
+def is_perfect_square(number):
+    square = number ** 2
+    return square * square == number
+
+def is_number_fibonnaci(numberToFind):
+    if numberToFind == 0:
+        return True
+    return is_perfect_square(5 * numberToFind * numberToFind + 4) or is_perfect_square(5 * numberToFind * numberToFind - 4)
         
-fibonnaci(100)
+for i in range(10):
+    if is_number_fibonnaci(i) == True:
+        print("{} is fibonnaci".format(i))
+    else:
+        print("{} not fibonnaci".format(i))
